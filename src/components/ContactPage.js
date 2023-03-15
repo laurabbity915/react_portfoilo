8a812045673ab2161c412f0900538cff2e79d569
laurabbity915 <laura_yuen915@outlook.com>
2023-03-15 00:05:40 +0000

    added contact form

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,11 +1,39 @@
+import { Button, TextField } from '@mui/material';
+import { Container } from '@mui/system';
+import React, { useState } from 'react';
 
-import React from 'react';
+function ContactForm() {
 
-export default function ContactPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmit = () => {
+    console.log(name);
+    console.log(message);
+    console.log(email);
+  };
 
   return (
     <>
-      <h1>this is ContactPage</h1>
+      <Container>
+        <Container className='text-field'>
+          <TextField label="name" name="name" value={name} onChange={(event) => setName(event?.target?.value)} />
+        </Container>
+        <Container className='text-field'>
+          <TextField label="Email" name="email" value={email} onChange={(event) => setEmail(event?.target?.value)} />
+        </Container>
+        <Container className='text-field'>
+          <TextField label="Message" name="message" value={message} onChange={(event) => setMessage(event?.target?.value)} multiline rows={4} />
+        </Container>
+        <Container>
+          <Button type="submit" variant="contained" onClick={onSubmit} color="primary">Submit</Button>
+        </Container>
+      </Container>
     </>
+
+
   );
 }
+
+export default ContactForm;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,5 @@
 
-import { Container } from '@mui/system';
+import { Container, Box } from '@mui/system';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -9,11 +9,24 @@ export default function Project() {
   console.log(location);
   return (
     <>
-      <Container>
-        <h1>{location.state.project.title}</h1>
-        <span>{location.state.project.desc}</span>
-        <Link to={location.state.project.url}> Demo </Link>
-        <Link to={location.state.project.github}>Github</Link>
+      <Container className='projectDetails'>
+        <Box 
+        component="img"
+        src={location.state.project.image}
+        className="project-image"
+        
+        />
+        <Container><h1>{location.state.project.title}</h1>
+          <span>{location.state.project.desc}</span></Container>
+
+        <Container>
+          <span>click here and find the Demo:</span>
+          <Link to={location.state.project.url}> Demo </Link>
+          <br/>
+          <spam>click here and go to my Github:</spam>
+          <Link to={location.state.project.github}>Github</Link>
+        </Container>
+
       </Container>
     </>
   );
